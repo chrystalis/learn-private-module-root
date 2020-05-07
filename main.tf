56cb5002718470527cac852b2432e43d1ed508e3
Christina Plummer <christina.plummer@cbrands.com>
2020-05-07 10:29:32 -0400

    Updated config to use module from cplummer1-private

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+#  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/cplummer1-private/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
